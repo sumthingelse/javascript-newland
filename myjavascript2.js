53b750c316cad55a287e3a14ed638bb7298e6e7d
Donna R Peterson <sumthingelse@users.noreply.github.com>
2016-08-28 00:02:27 -0700

    Update myjavascript2.js

--- myjavascript2.js
+++ myjavascript2.js
@@ -4,7 +4,8 @@ var a;
 a = 5000;
 alert (a);
 var phrase = "This is a simple phrase.";
-alert (phrase.length);
+alert (phrase.length); //this gives the length of the variable phrase
 var aa =100;
 var bb = 50;
 var result = var aa - var bb;
+//these all same -> a = a + 1; a += 1; a++; add one to a variable
