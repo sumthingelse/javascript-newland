49b459c805159992ff6964e1c14016b372efbc59
Donna R Peterson <sumthingelse@users.noreply.github.com>
2016-08-29 16:29:48 -0700

    Update myjavascript2.js

--- myjavascript2.js
+++ myjavascript2.js
@@ -9,3 +9,23 @@ var aa =100;
 var bb = 50;
 var result = var aa - var bb;
 //these all same -> a = a + 1; a += 1; a++; add one to a variable
+
+//now creating a simple array, zero based
+var simplearray = [];
+simplearray = [50, 60, 70, 80];
+alert( simplearray.length);
+simplearray.reverse (); //reversing the objects in the array
+simplearray.sort(); //sorting the objects in the array
+simplearray.join (); //joining the objects in an array
+
+/* To see a Javascript reference guide go to
+https://develper.mozilla.org/en/Javascript/Reference 
+*/
+
+//interate through all elements in an array is useful, but start index at zero
+var i = 0;
+while (i< simplearray.length) {
+  alert ("the value is: simplearray [i]);
+  i++;
+}
+
