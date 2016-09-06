91f400b3ce956ae4e0e66736a256484ee3c9a888
Donna R Peterson <sumthingelse@users.noreply.github.com>
2016-09-05 19:22:19 -0700

    Create javascriptDom.js

---
+++ javascriptDom.js
@@ -0,0 +1,7 @@
+//repsonding to a user click on a headline
+var headline = document.getElementById
+("mainHeading");
+
+headline.onclick = function() {
+  headline.innerHTML - "you clicked the headline.";
+  };
